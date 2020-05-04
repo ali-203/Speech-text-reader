b20ad2d8342fd4a52f21a77d20000b90d5481c8e
ali-203 <ludhianvi0307@gmail.com>
2020-05-04 06:43:17 +0500

    update

---
+++ script.js
@@ -0,0 +1,58 @@
+
+const main = document.querySelector('main');
+const voicesSelect = document.getElementById('voices');
+const textarea = document.getElementById('text');
+const readBtn = document.getElementById('read');
+const toggleBtn = document.getElementById('toggle');
+const closeBtn = document.getElementById('close');
+
+const data = [
+ 
+    {
+        image: './img/drink.jpg',
+        text: "I'm Thirsty"
+    },
+    {
+        image: './img/food.jpg',
+        text: "I'm Hungry"
+    },
+
+    {
+        image: './img/tired.jpg',
+        text: "I'm Tired"
+    },
+
+    {
+        image: './img/hurt.jpg',
+        text: "I'm Hurt"
+    },
+
+    {
+        image: './img/drink.jpg',
+        text: "I'm Thirsty"
+    },
+    {
+        image: './img/drink.jpg',
+        text: "I'm Thirsty"
+    },
+
+    {
+        image: './img/drink.jpg',
+        text: "I'm Thirsty"
+    },
+    {
+        image: './img/drink.jpg',
+        text: "I'm Thirsty"
+    },
+    {
+        image: './img/drink.jpg',
+        text: "I'm Thirsty"
+    },
+
+
+
+
+
+
+
+]
